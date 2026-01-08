c1270bf19a90114e39d5be1fb125bbb7f4b84b95
f24ba063 <f24ba063@chuo.ac.jp>
2026-01-09 08:24:00 +0900

    '商品コントロールに、既存データ編集のためのPutMapping追加'

--- src/main/java/com/example/warehouse/controller/masterController/ProductMasterController.java
+++ src/main/java/com/example/warehouse/controller/masterController/ProductMasterController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,16 +25,31 @@ public class ProductMasterController {
 		this.productService = productService;
 	}
 
+	//データ獲得処理
 	@GetMapping
 	public List<Product> getAllProducts(){
 		return productService.getAllProducts();
 	}
 	
+	//新データの挿入処理
 	@PostMapping
 	public Product addP(@RequestBody Product product) {
 		return productService.addProduct(product);
 	}
 	
+	//既存データの編集処理
+    @PutMapping("{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        Product existing = productService.findById(id);
+        if(existing != null) {
+            // IDを上書きしないように注意
+            product.setProductId(id);
+            return productService.saveProduct(product);
+        }
+        return null; // or throw exception
+    }
+    
+    //ソフトデリート処理
 	@PatchMapping("{id}/softDelete")
 	public void softDeleteProduct(@PathVariable Long id){
 		Product product = productService.findById(id);
